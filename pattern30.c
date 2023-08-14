54a6917a784e8847320c769328f6e82141365017
Sripriya Sharma <139264751+sri510@users.noreply.github.com>
2023-08-14 21:45:30 +0530

    Create pattern30.c

---
+++ pattern30.c
@@ -0,0 +1,22 @@
+/*
+17 18 19 20
+
+14 15 16
+
+12 13
+
+11  
+*/
+#include<stdio.h>
+int main(){
+    int n=4,i,j,k=n*n;
+    for(i=n;i>=1;i--)
+    {
+        for(j=1;j<=i;j++)
+        {
+            printf("%3d",++k);
+        }    
+        k=k-(2*i)+1;
+        printf("\n");
+    }
+}
